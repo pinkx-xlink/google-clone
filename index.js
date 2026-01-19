12ecc573e2e613478427bdb35acd9759f01fb178
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2026-01-18 19:21:23 -0500

    Create searchGoogle function, which will actually open a new tab with the Google search results

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const search = document.getElementById('search-bar').value;
-document.getElementById('search-bar').addEventListener('keydown', function(e) {
-  if (e.key === 'Enter') {
-    // e.preventDefault();
-    console.log('key pressed')
-    window.location.href = `https://www.google.com/search?q=${search}&tbm=isch`;
-    }
-})
+const search = document.getElementById("search-bar").value;
+document.getElementById("search-bar").addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    searchGoogle();
+  }
+});
+
+function searchGoogle() {
+  const query = document.getElementById("search-bar").value;
+
+  // check if input is empty
+
+  if (!query) {
+    alert("Please enter a search term.");
+    return;
+  }
+
+  const searchParams = new URLSearchParams({ q: query });
+  const queryString = searchParams.toString();
+
+  const googleSearchUrl = `https://www.google.com/search?${queryString}&tbm=isch`;
+  window.open(googleSearchUrl, "_blank");
+}
